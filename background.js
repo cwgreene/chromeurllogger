0a1077c68eddc90689b2136717aa9b6670d563b4
Chris Greene <archgoon@gmail.com>
2017-02-11 14:20:03 -0800

    Apparently forgot to commit background.js
    Oops.

---
+++ background.js
@@ -0,0 +1,35 @@
+console.log("Doom");
+function post(data, settings) {
+  // Be sure this is all done securely!
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", settings.backend_url);
+  xhr.onreadystatechange = function() {
+    console.log(xhr);
+    if(xhr.readystate == 4) {
+      console.log(xhr.responseText);
+    }
+  }
+  xhr.send(JSON.stringify({
+    username: settings.username,
+    credentials: settings.credentials,
+    data: data
+  }));
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.query({active: true, 'lastFocusedWindow': true}, function(tabs) {
+      var activeTab = tabs[0];
+      var url = tabs[0].url;
+      //debugging
+      console.log(url);
+  
+
+    chrome.storage.sync.get({
+      backend_url: '',
+      username: '',
+      credentials: ''
+    }, function(settings) {
+      post(url, settings);
+    });
+  });
+});
